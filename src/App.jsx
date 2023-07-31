5854fac7ec2430ef719a853f3f476c7be01397fa
Eman <emanamberzindani@gmail.com>
2023-07-31 15:53:56 -0500

    added QR code

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,30 @@
+import QRCode from 'qrcode'
+import { useState } from 'react'
 
 
 function App() {
+  const [url, setUrl] = useState('')
+  const [qrcode, setQrcode] = useState('')
+
+  const GenerateQRCode = () => {
+    QECode.toDataURL(url,(err,url) => {
+      if(err) return console.error(err)
+
+      console.log(url)
+      setQrcode(url)
+    })
+  }
+
   return (
     <div className="app">
       <h1>QR Code Generator</h1>
       <input
         type="text"
-        placeholder="e.g. https://google.com" />
-      <button>Generate</button>
+        placeholder="e.g. https://google.com"
+        value={url}
+        onChange={(evt) => setUrl(evt.target.value)} />
+      <button onClick={GenerateQRCode}>Generate</button>
+      <img src={qrcode} />
     </div>
   )
 }
