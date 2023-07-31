4fb2baf106cebde079df2bb0ba3d0570e9126b70
Eman <emanamberzindani@gmail.com>
2023-07-31 15:45:43 -0500

    styling done

---
+++ src/App.jsx
@@ -0,0 +1,15 @@
+
+
+function App() {
+  return (
+    <div className="app">
+      <h1>QR Code Generator</h1>
+      <input
+        type="text"
+        placeholder="e.g. https://google.com" />
+      <button>Generate</button>
+    </div>
+  )
+}
+
+export default App
